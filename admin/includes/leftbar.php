628c9bd439e97cc5b93bbfe5c14a3a60486d07a4
Yusuf Bozkurt <ysfbzkrt2015@gmail.com>
2025-05-12 10:54:34 +0300

    admin paneli için leftbar.php translate edildi

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -1,40 +1,40 @@
 	<nav class="ts-sidebar">
 			<ul class="ts-sidebar-menu">
 			
-				<li class="ts-label">Main</li>
-				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li class="ts-label">Ana Sayfa</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Kontrol Paneli</a></li>
 			
-<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<li><a href="#"><i class="fa fa-files-o"></i> Araç Markası</a>
 <ul>
-<li><a href="create-brand.php">Create Brand</a></li>
-<li><a href="manage-brands.php">Manage Brands</a></li>
+<li><a href="create-brand.php">Marka Oluştur</a></li>
+<li><a href="manage-brands.php">Markaları Yönet</a></li>
 </ul>
 </li>
 
-<li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
+<li><a href="#"><i class="fa fa-car"></i>Araçlar</a>
 					<ul>
-						<li><a href="post-avehical.php">Post a Vehicle</a></li>
-						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+						<li><a href="post-avehical.php">Araç Paylaş</a></li>
+						<li><a href="manage-vehicles.php">Araçları Yönet</a></li>
 					</ul>
 				</li>
 
-<li><a href="#"><i class="fa fa-sitemap"></i> Bookings</a>
+<li><a href="#"><i class="fa fa-sitemap"></i> Rezervasyonlar</a>
 					<ul>
-						<li><a href="new-bookings.php">New</a></li>
-						<li><a href="confirmed-bookings.php">Confirmed</a></li>
-						<li><a href="canceled-bookings.php">Canceled</a></li>
+						<li><a href="new-bookings.php">Yeni</a></li>
+						<li><a href="confirmed-bookings.php">Onaylanan</a></li>
+						<li><a href="canceled-bookings.php">İptal Edilen</a></li>
 					</ul>
 				</li>
 
 		
 
-				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
-				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Conatctus Query</a></li>
-				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
-			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
-			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Görüşleri Yönet</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> İletişim Sorgularını Yönet</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i>Kayıtlı Kullanıcılar</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Sayfaları Yönet </a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i>İletişim Bilgilerini Güncelle</a></li>
 
-			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Aboneleri Yönet</a></li>
 
 			</ul>
 		</nav>
