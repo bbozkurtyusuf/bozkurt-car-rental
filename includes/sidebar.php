1c815b85732f9a7cda490cd745b83a9594acbd8b
Yusuf Bozkurt <ysfbzkrt2015@gmail.com>
2025-05-12 10:45:36 +0300

    sidebar.php translate edildi

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,11 +1,11 @@
 <div class="profile_nav">
           <ul>
-            <li><a href="profile.php">Profile Settings</a></li>
-              <li><a href="update-password.php">Update Password</a></li>
-            <li><a href="my-booking.php">My Booking</a></li>
-            <li><a href="post-testimonial.php">Post a Testimonial</a></li>
-               <li><a href="my-testimonials.php">My Testimonials</a></li>
-            <li><a href="logout.php">Sign Out</a></li>
+            <li><a href="profile.php">Profil Ayarları</a></li>
+              <li><a href="update-password.php">Şifre Güncelle</a></li>
+            <li><a href="my-booking.php">Rezervasyonlarım</a></li>
+            <li><a href="post-testimonial.php">Görüşlerinizi Önemsiyoruz</a></li>
+               <li><a href="my-testimonials.php">Görüşlerim</a></li>
+            <li><a href="logout.php">Çıkış Yap</a></li>
           </ul>
         </div>
       </div>
