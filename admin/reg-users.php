346a3b841a3bbd7ddb6e31fdb712afd26c28cc01
Yusuf Bozkurt <ysfbzkrt2015@gmail.com>
2025-05-14 15:54:48 +0300

    reg-users.php translate edildi

--- admin/reg-users.php
+++ admin/reg-users.php
@@ -33,7 +33,7 @@ $msg="Page data updated  successfully";
 	<meta name="author" content="">
 	<meta name="theme-color" content="#3e454c">
 	
-	<title>Car Rental Portal |Admin Manage testimonials   </title>
+	<title>Araç Kiralama Portalı | Yönetici Görüş Yönetimi   </title>
 
 	<!-- Font awesome -->
 	<link rel="stylesheet" href="css/font-awesome.min.css">
@@ -83,11 +83,11 @@ $msg="Page data updated  successfully";
 				<div class="row">
 					<div class="col-md-12">
 
-						<h2 class="page-title">Registered Users</h2>
+						<h2 class="page-title">Kayıtlı Kullanıcılar</h2>
 
 						<!-- Zero Configuration Table -->
 						<div class="panel panel-default">
-							<div class="panel-heading">Reg Users</div>
+							<div class="panel-heading">Kayıtlı Kullanıcılar</div>
 							<div class="panel-body">
 							<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
 				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
@@ -95,29 +95,31 @@ $msg="Page data updated  successfully";
 									<thead>
 										<tr>
 										<th>#</th>
-												<th> Name</th>
-											<th>Email </th>
-											<th>Contact no</th>
-										<th>DOB</th>
-										<th>Address</th>
-										<th>City</th>
-										<th>Country</th>
-										<th>Reg Date</th>
+										<th>İsim</th>
+										<th>E-Mail Adresi </th>
+										<th>İletişim Numarası</th>
+										<th>Doğum Tarihi</th>
+										<th>Adres</th>
+										<th>Şehir</th>
+										<th>Ülke</th>
+										<th>Kayıt Tarihi</th>
 										
 										</tr>
 									</thead>
 									<tfoot>
 										<tr>
 										<th>#</th>
-											<th> Name</th>
-											<th>Email </th>
-											<th>Contact no</th>
-										<th>DOB</th>
-										<th>Address</th>
-										<th>City</th>
-										<th>Country</th>
-										<th>Reg Date</th>
+										<th>İsim</th>
+										<th>E-Mail Adresi </th>
+										<th>İletişim Numarası</th>
+										<th>Doğum Tarihi</th>
+										<th>Adres</th>
+										<th>Şehir</th>
+										<th>Ülke</th>
+										<th>Kayıt Tarihi</th>
+										
 										</tr>
+										
 										</tr>
 									</tfoot>
 									<tbody>
