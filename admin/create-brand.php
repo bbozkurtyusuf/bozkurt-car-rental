ea12fc9ed4d5efe3e9f49843235a190f6024fd8a
Yusuf Bozkurt <ysfbzkrt2015@gmail.com>
2025-05-14 14:45:39 +0300

    create-brand.php translate edildi

--- admin/create-brand.php
+++ admin/create-brand.php
@@ -39,7 +39,7 @@ $error="Something went wrong. Please try again";
 	<meta name="author" content="">
 	<meta name="theme-color" content="#3e454c">
 	
-	<title>Car Rental Portal | Admin Create Brand</title>
+	<title>Araç Kiralama Portalı | Yönetici Marka Oluştur</title>
 
 	<!-- Font awesome -->
 	<link rel="stylesheet" href="css/font-awesome.min.css">
@@ -89,12 +89,12 @@ $error="Something went wrong. Please try again";
 				<div class="row">
 					<div class="col-md-12">
 					
-						<h2 class="page-title">Create Brand</h2>
+						<h2 class="page-title">Marka Oluştur</h2>
 
 						<div class="row">
 							<div class="col-md-10">
 								<div class="panel panel-default">
-									<div class="panel-heading">Create Brand</div>
+									<div class="panel-heading">Marka Oluştur</div>
 									<div class="panel-body">
 										<form method="post" name="chngpwd" class="form-horizontal" onSubmit="return valid();">
 										
@@ -102,7 +102,7 @@ $error="Something went wrong. Please try again";
   	        	  <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
 				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
 											<div class="form-group">
-												<label class="col-sm-4 control-label">Brand Name</label>
+												<label class="col-sm-4 control-label">Marka Adı</label>
 												<div class="col-sm-8">
 													<input type="text" class="form-control" name="brand" id="brand" required>
 												</div>
@@ -115,7 +115,7 @@ $error="Something went wrong. Please try again";
 											<div class="form-group">
 												<div class="col-sm-8 col-sm-offset-4">
 								
-													<button class="btn btn-primary" name="submit" type="submit">Submit</button>
+													<button class="btn btn-primary" name="submit" type="submit">Gönder</button>
 												</div>
 											</div>
 
